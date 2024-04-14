ce8be3e5ed6f16df3e213d04c3ca7bfb4d623fb2
Andrelise28 <andrelisebuttner@hotmail.com>
2024-04-14 20:32:01 -0300

    Ajustes na tela de novo pedido

--- trabalho_A1_02/resources/views/pedidos/novo.blade.php
+++ trabalho_A1_02/resources/views/pedidos/novo.blade.php
@@ -1,36 +1,26 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Avaliação A1/2</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novo Pedido</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
-<body class="p-3 m-0 border-0 bd-example m-0 border-0">
-    <div class="container">
-        <div class="row">
-            <h2>Novo pedido</h2>
-        </div>
+<body>
+    <div class="container mt-5">
+        <h2>Novo pedido</h2>
         <form>
-            <div class="row mb-3">
-                <div class="form-group">
-                    <label for="dataPedido">Data</label>
-                    <input type="date" class="form-control" id="dataPedido">
-                </div>
+            <div class="form-group">
+                <label for="data">Data</label>
+                <input type="date" class="form-control" id="data" name="data">
             </div>
-            <div class="row gap-3">
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Salvar</button>
-                    <button type="button" class="btn btn-info">Voltar</button>
-                </div>
-            </div>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+            <button type="button" class="btn btn-secondary">Voltar</button>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
