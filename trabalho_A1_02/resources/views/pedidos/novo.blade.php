7799ef4ba0c724114031350e1113d476942f994e
Andrelise28 <andrelisebuttner@hotmail.com>
2024-04-14 20:15:56 -0300

    front da tela de criação de pedidos

---
+++ trabalho_A1_02/resources/views/pedidos/novo.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Avaliação A1/2</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <div class="container">
+        <div class="row">
+            <h2>Novo pedido</h2>
+        </div>
+        <form>
+            <div class="row mb-3">
+                <div class="form-group">
+                    <label for="dataPedido">Data</label>
+                    <input type="date" class="form-control" id="dataPedido">
+                </div>
+            </div>
+            <div class="row gap-3">
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <button type="button" class="btn btn-info">Voltar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
